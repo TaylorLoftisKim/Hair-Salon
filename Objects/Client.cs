57fa095ee2c7840b2ce14bffbe6346e14e026fca
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-12-12 17:32:51 -0800

    make variables within Objects

---
+++ Objects/Client.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Salon.Objects
+{
+  public class Client
+  {
+    private int _id;
+    public string _name;
+    public string _phone;
+    public string _style;
+  }
+}
---
+++ Objects/Stylist.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Salon.Objects
+{
+  public class Stylist
+  {
+    private int _id;
+    public string _name;
+    public string _details;
+    public int _clientId;
+  }
+
+  public Stylist(string stylistName, string stylistDetails, int stylistClientId, int id = 0)
+  {
+    _name = stylistName;
+    _details = stylistDetails;
+    _clientId = stylistClientId;
+    _id = id;
+  }
+
+  public override bool Equals(System.Object otherStylist)
+  {
+    if(!(otherStylist is Stylist))
+    {
+      return false;
+    }
+  }
+}
