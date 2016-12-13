be099954716de2ba89e69cdb3c71fc03a226d200
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-12-12 22:25:54 -0800

    create multiple tests for Stylist object, currently passing

--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -1,1 +1,34 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using Review.Objects;
 
+namespace Salon
+{
+  public class StylistTest : IDisposable
+  {
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=stylist_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_DatabaseEmptyAtFirst()
+    {
+      int result = Stylist.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+    [Fact]
+    public void Test_Save_SavesToDatabase()
+    {
+      Stylist testStylist = new Stylist("Mary", 1);
+
+      testStylist.Save();
+      List<Stylist> result = Stylist.GetAll();
+      List<Stylist> testList = new List<Stylist>{testStylist};
+
+      Assert.Equal(testList, result);
+    }
+  }
+}
